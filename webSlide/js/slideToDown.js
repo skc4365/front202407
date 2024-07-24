40363791bac70ee5779e3087410772e01c1a6e3a
skc4365 <skc4365@gmail.com>
2024-07-24 15:12:04 +0900

    메인슬라이드_좌우위아래
    이미지자료는 없음

---
+++ webSlide/js/slideToDown.js
@@ -0,0 +1,21 @@
+var slide = $(".slideContent>img");
+var startCnt = 0;
+var endCnt = slide.length - 1;
+
+var timer = setInterval("autoSlide()", 3000);
+
+function autoSlide() {
+    $(slide[startCnt]).stop().animate(
+        { top: "300px" }, 1000, function () {
+            $(this).css({ top: "-300px" });
+        }
+    );
+    startCnt++;
+    if (startCnt > endCnt) {
+        startCnt = 0;
+    }
+    $(slide[startCnt]).stop().animate(
+        { top: "0" }, 1000);
+}
+
+
---
+++ webSlide/js/slideToLeft.js
@@ -0,0 +1,21 @@
+var slide = $(".slideContent>img");
+var startCnt = 0;
+var endCnt = slide.length - 1;
+
+var timer = setInterval("autoSlide()", 3000);
+
+function autoSlide() {
+    $(slide[startCnt]).stop().animate(
+        { left: "-100%" }, 1000, function () {
+            $(this).css({ left: "100%" });
+        }
+    );
+    startCnt++;
+    if (startCnt > endCnt) {
+        startCnt = 0;
+    }
+    $(slide[startCnt]).stop().animate(
+        { left: "0" }, 1000);
+}
+
+
